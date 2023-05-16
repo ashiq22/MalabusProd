73b39254276bb07c924604dd24273cec69d81a53
TechnogmMalabus <130576090+TechnogmMalabus@users.noreply.github.com>
2023-05-16 16:40:09 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 //connect data
-mongoose.connect('mongodb+srv://malabus:devopsdevops@malabus.wp0djog.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://malabus:devopsdevops@malabus.wp0djog.mongodb.net/malabus', {
     useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log('Connected to MongoDB'))
